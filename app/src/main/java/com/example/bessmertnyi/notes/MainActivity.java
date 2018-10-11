5c56ffd2aad66296d62c29873181eaf28c886db4
besmertn <besmertn@bk.ru>
2018-10-11 12:18:56 +0300

    normal appBar

--- app/src/main/java/com/example/bessmertnyi/notes/MainActivity.java
+++ app/src/main/java/com/example/bessmertnyi/notes/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
@@ -47,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         notesRecyclerView.setAdapter(notesAdapter);
 
         loadNotes();
-
     }
 
     @Override
