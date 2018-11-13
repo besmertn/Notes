9168df5a899a5a2d78d36ef3850c6b9b43263934
besmertn <besmertn@bk.ru>
2018-11-13 13:45:11 +0200

    audio files loading

---
+++ app/src/main/java/com/example/bessmertnyi/notes/AudioModel.java
@@ -0,0 +1,41 @@
+package com.example.bessmertnyi.notes;
+
+public class AudioModel {
+    private String aPath;
+    private String aName;
+    private String aAlbum;
+    private String aArtist;
+
+    public String getaPath() {
+        return aPath;
+    }
+
+    public void setaPath(String aPath) {
+        this.aPath = aPath;
+    }
+
+    public String getaName() {
+        return aName;
+    }
+
+    public void setaName(String aName) {
+        this.aName = aName;
+    }
+
+    public String getaAlbum() {
+        return aAlbum;
+    }
+
+    public void setaAlbum(String aAlbum) {
+        this.aAlbum = aAlbum;
+    }
+
+    public String getaArtist() {
+        return aArtist;
+    }
+
+    public void setaArtist(String aArtist) {
+        this.aArtist = aArtist;
+    }
+}
+
--- app/src/main/java/com/example/bessmertnyi/notes/MainActivity.java
+++ app/src/main/java/com/example/bessmertnyi/notes/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.bessmertnyi.notes;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,11 +42,11 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final int CREATE_NEW_NOTE = 1;
     private static final int EDIT_NOTE = 2;
+    private static final int RUNTIME_PERMISSION_CODE = 3;
     private int selectedNotePosition;
     private NoteAdapter notesAdapter;
     private Spinner spinner;
-    /*private AppDatabase db;
-    private NoteDao noteDao;*/
+
 
     @Override
     protected void onStart() {
@@ -92,6 +102,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         notesAdapter.setNotesList(notes);
                     }
                 });
+
+        AndroidRuntimePermission();
+
+        ArrayList<AudioModel> lst = new ArrayList<>(getAllAudioFromDevice());
+        System.out.println(lst.toString());
     }
 
 
@@ -199,6 +214,128 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    /*public void GetAllMediaMp3Files(){
+
+        contentResolver = this.getContentResolver();
+
+        uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+        cursor = contentResolver.query(
+                uri, // Uri
+                null,
+                null,
+                null,
+                null
+        );
+
+        if (cursor == null) {
+
+            Toast.makeText(MainActivity.this,"Something Went Wrong.", Toast.LENGTH_LONG);
+
+        } else if (!cursor.moveToFirst()) {
+
+            Toast.makeText(MainActivity.this,"No Music Found on SD Card.", Toast.LENGTH_LONG);
+
+        }
+        else {
+
+            int Title = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+
+            //Getting Song ID From Cursor.
+            //int id = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+
+            do {
+
+                // You can also get the Song ID using cursor.getLong(id).
+                //long SongID = cursor.getLong(id);
+
+                String SongTitle = cursor.getString(Title);
+
+                // Adding Media File Names to ListElementsArrayList.
+                ListElementsArrayList.add(SongTitle);
+
+            } while (cursor.moveToNext());
+        }
+    }*/
+
+
+    // Creating Runtime permission function.
+    public void AndroidRuntimePermission() {
+
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                AlertDialog.Builder alert_builder = new AlertDialog.Builder(MainActivity.this);
+                alert_builder.setMessage("External Storage Permission is Required.");
+                alert_builder.setTitle("Please Grant Permission.");
+                alert_builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        ActivityCompat.requestPermissions(
+                                MainActivity.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                RUNTIME_PERMISSION_CODE
+                        );
+                    }
+                });
+
+                alert_builder.setNeutralButton("Cancel", null);
+                AlertDialog dialog = alert_builder.create();
+                dialog.show();
+            } else {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        RUNTIME_PERMISSION_CODE
+                );
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case RUNTIME_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                }
+            }
+        }
+    }
+
+    public List<AudioModel> getAllAudioFromDevice() {
+        final List<AudioModel> tempAudioList = new ArrayList<>();
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Audio.AudioColumns.DATA,
+                MediaStore.Audio.AudioColumns.TITLE,
+                MediaStore.Audio.AudioColumns.ALBUM,
+                MediaStore.Audio.ArtistColumns.ARTIST,};
+        Cursor c = this.getContentResolver().query(uri, projection, null, null, null);
+        if (c != null) {
+            while (c.moveToNext()) {
+                AudioModel audioModel = new AudioModel();
+                String path = c.getString(0);
+                String name = c.getString(1);
+                String album = c.getString(2);
+                String artist = c.getString(3);
+                audioModel.setaName(name);
+                audioModel.setaAlbum(album);
+                audioModel.setaArtist(artist);
+                audioModel.setaPath(path);
+                Log.e("Name :" + name, " Album :" + album);
+                Log.e("Path :" + path, " Artist :" + artist);
+                tempAudioList.add(audioModel);
+            }
+            c.close();
+        }
+        return tempAudioList;
     }
 }
 
